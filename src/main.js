74a3db5bc83a9c7bfe96362ec995f8d95c19861b
YWJ <446460840@qq.com>
2019-03-24 18:01:03 +0800

    完成home

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 // 入口文件
 import Vue from 'vue';
 
-import './lib/mui/css/mui.min.css';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 
-import {Header} from 'mint-ui';
-Vue.component(Header.name,Header);
+import './lib/mui/css/mui.min.css';
+import './lib/mui/css/icons-extra.css';
+
+import {Header,Swipe,SwipeItem} from 'mint-ui';
+Vue.component(Header.name,Header);//Header.name这是mint-ui的Header标签的名字，可以直接使用
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+import vueResource from 'vue-resource';
+Vue.use(vueResource);
+
+import router from './router.js';
 
 //最后导入App.vue
 import app from './App.vue';
@@ -12,5 +23,6 @@ import app from './App.vue';
 
 var vm=new Vue({
   el:'#app',
-  render:c=>c(app)
+  render:c=>c(app),
+  router
 });
--- src/router.js
+++ src/router.js
@@ -1,11 +1,21 @@
-import VueRouter from 'vue-router'
+import VueRouter from 'vue-router';
 
+import homeContainer from './components/tabbar/homeContainer.vue';
+import shopcartContainer from './components/tabbar/shopcartContainer.vue';
+import memberContainer from './components/tabbar/memberContainer.vue';
+import searchContainer from './components/tabbar/searchContainer.vue';
 
 // 3. 创建路由对象
 var router = new VueRouter({
   routes: [
-  ]
-})
+    {path:'/',redirect:'/home'},
+    {path:'/home',component:homeContainer},
+    {path:'/shopcart',component:shopcartContainer},
+    {path:'/member',component:memberContainer},
+    {path:'/search',component:searchContainer}
+  ],
+  linkActiveClass:'mui-active'
+});
 
 // 把路由对象暴露出去
-export default router
+export default router;
